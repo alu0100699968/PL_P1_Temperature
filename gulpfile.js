5cfccb68b1b55f4229ecc448c12276d18b6414df
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-02-25 19:38:34 +0000

    Añadida tarea a gulpfile para lanzar los tests con Karma

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ concat  = require('gulp-concat');
 var del     = require('del');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
+var karma = require('karma').server;
 
 gulp.task('minify', function () {
   gulp.src('temperature.js')
@@ -22,4 +23,11 @@ gulp.task('minify', function () {
 
 gulp.task('clean', function(cb) {
   del(['minified/*'], cb);
+});
+
+gulp.task('tests', function(done) {
+  return karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
