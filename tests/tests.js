0e326dca91eb90217f8c2c79d9c818de820a92c6
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-02-25 20:57:09 +0000

    Añadido soporte de Blanket

--- tests/tests.js
+++ tests/tests.js
@@ -20,6 +20,16 @@ suite('temperature', function() {
     calculate();
     assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
   });
+  test('25e4F = 138871.1C', function() {
+    original.value = "25e4F";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "138871.1 Celsius");
+  });
+  test('30e2C = 5432.0 Farenheit', function() {
+    original.value = "30e2C";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "5432.0 Farenheit");
+  });
   test('5X = error', function() {
     original.value = "5X";
     calculate();
